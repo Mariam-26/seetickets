a9d141ad20632e3c7545ddfd347d94d10f80e608
Oxydase <figueiredo.milton16@gmail.com>
2024-03-05 17:01:58 +0100

    mise en place de la fonction reservation d'une place (en cours)

--- src/Controller/TicketsController.php
+++ src/Controller/TicketsController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Programmation;
+use App\Entity\Ticket;
+use App\Repository\ProgrammationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,4 +18,33 @@ class TicketsController extends AbstractController
             'controller_name' => 'TicketsController',
         ]);
     }
+
+    #[Route('/tickets/create/{id}', name: 'app_tickets')]
+    public function createTicket(ProgrammationRepository $prog, string $id): Response
+    {
+        $programmation = $prog->findOneBy(['id'=> $id]);
+        // print_r($programmation->getId());
+        $user=$this->getUser();
+        var_dump($user->getUserFirstname());
+        $ticket = new Ticket;
+
+        $ticket->setPrice('45');
+        $ticket->setTicketDate(new DateTime('now'));
+        $ticket->setTicketFirstname($user->getUserFirstname());
+        $ticket->setTicketLastname($user->getUserLastname());
+        $ticket->setTicketTime();
+        $ticket->setTicketPlace();
+        $ticket->setTicketNumberPlace();
+        $ticket->setProgrammation();
+        $ticket->setUser();
+
+
+
+        return $this->render('tickets/createTicket.html.twig', [
+            'controller_name' => 'TicketsController',
+            'programmation' => $programmation,
+            'user' =>$user
+        ]);
+    }
+
 }
