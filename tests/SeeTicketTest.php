d3496a873ccfd097d46bcafd1914e44400523c50
renatakaczor <renatakaczor@sfr.fr>
2024-03-05 16:42:14 +0100

    modif contoller Ticket et Events

---
+++ tests/SeeTicketTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class SeeTicketTest extends TestCase
+{
+    private $SeeTicket;
+    public function setUp(): void
+    {
+        //$this->assertTrue(true);
+        //$this->SeeTicket = new SeeTickets();
+    }
+}
