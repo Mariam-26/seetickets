e0a4ced9a388104e5a47af54d4d15a93b079fb6c
renatakaczor <renatakaczor@sfr.fr>
2024-03-05 11:46:19 +0100

    templating tickets

--- src/Controller/TicketsController.php
+++ src/Controller/TicketsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TicketRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class TicketsController extends AbstractController
 {
     #[Route('/tickets', name: 'app_tickets')]
-    public function index(): Response
+    public function index(TicketRepository $tickets): Response
     {
+        $t=$tickets->findAll();
         return $this->render('tickets/index.html.twig', [
             'controller_name' => 'TicketsController',
+            'tickets'=>$t
         ]);
     }
 }
