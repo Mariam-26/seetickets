97ce1b22baacd5a0a068b14e48a0612a0629c164
Oxydase <figueiredo.milton16@gmail.com>
2024-03-06 16:51:39 +0100

    réservation d'une place (fini)

--- src/Controller/TicketsController.php
+++ src/Controller/TicketsController.php
@@ -3,12 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Ticket;
+use App\Form\TicketType;
 use App\Repository\ProgrammationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class TicketsController extends AbstractController
 {
@@ -20,33 +22,52 @@ class TicketsController extends AbstractController
         ]);
     }
 
+    // Réservation d'une place 
     #[Route('/tickets/create/{id}', name: 'app_tickets')]
-    public function createTicket(ProgrammationRepository $prog, EntityManagerInterface $entityManager, string $id): Response
+    public function createTicket(ProgrammationRepository $prog, EntityManagerInterface $entityManager, Request $request, string $id): Response
     {
+        // Récupèration des infos de la programmation lié a l'id reçu en get
         $programmation = $prog->findOneBy(['id'=> $id]);
         // print_r($programmation->getId());
-        $user=$this->getUser();
-        var_dump($user->getUserFirstname());
-        $ticket = new Ticket;
 
-        $ticket->setPrice('45');
-        $ticket->setTicketDate(new DateTime('now'));
+        // A partir de la programmation on accède aux méthodes de l'entité event
+        $event= $programmation->getEvent();
+        // Récupération des infos de l'utilisateur connecté
+        $user=$this->getUser();
+        // var_dump($user->getUserFirstname());
+
+        // Instanciation d'un ticket
+        $ticket = new Ticket();
+        
+        // Attribution de valeurs au ticket
         $ticket->setTicketFirstname($user->getUserFirstname());
         $ticket->setTicketLastname($user->getUserLastname());
-        $ticket->setTicketTime(new DateTime('now'));
-        $ticket->setTicketPlace('Salle B');
-        $ticket->setTicketNumberPlace('A125');
-        $ticket->setProgrammation($programmation);
-        $ticket->setUser($user);
+        $ticket->setTicketNumberPlace('');
 
-        $entityManager->persist($ticket);
-        $entityManager->flush();
+        // Création du formulaire
+        $form=$this->createForm(TicketType::class,$ticket);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            // Attribution de valeurs au ticket (toutes les valeurs qui ne sont pas présentes dans le formulaire)
+            $ticket->setPrice($event->getEventPrice());
+            $ticket->setTicketTime(new DateTime('now'));
+            $ticket->setTicketDate($programmation->getProgrammationDate());
+            $ticket->setTicketPlace($event->getEventPlace());
+            $ticket->setProgrammation($programmation);
+            $ticket->setUser($user);
 
-
+            // Enregistrer les infos en BDD
+            $entityManager->persist($ticket);
+            $entityManager->flush();
+            // Redirection vers la page d'accueil
+            return $this->redirectToRoute('app_home');
+        }
+        
         return $this->render('tickets/createTicket.html.twig', [
             'controller_name' => 'TicketsController',
             'programmation' => $programmation,
-            'user' =>$user
+            'user' =>$user,
+            'form' => $form->createView()
         ]);
     }
 
