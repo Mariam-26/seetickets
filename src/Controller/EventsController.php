def4196e24a847c6a0ff490aa984f85f8814e329
demba75009 <b-demba@hotmail.fr>
2024-03-06 09:08:46 +0100

    maj

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -8,7 +8,6 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EventsController extends AbstractController
 {
